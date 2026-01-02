fa906d60d2f79314439d670ef20f3b0401957dc0
Do Hoai Anh <anhd78428@gmail.com>
2026-01-02 22:58:30 +0700

    Setup typeScript

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const PORT = 8080;
 app.get("/", (req, res) => {
     res.send("hello word")
 })
-app.get("/home", (req, res) => {
+app.get("/hazi_anhh", (req, res) => {
     res.send("hello Hazi_anhh")
 })
 app.listen(PORT, () => {
