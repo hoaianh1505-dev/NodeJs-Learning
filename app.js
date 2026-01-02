34634a66f8077c693d0d8881898d6866c9746513
Do Hoai Anh <anhd78428@gmail.com>
2026-01-02 22:40:01 +0700

    hello word with express

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-console.log("hello word")
-console.log("Hazi_anh dep try")
+const express = require("express");
+const app = express();
+const PORT = 8080;
+app.get("/", (req, res) => {
+    res.send("hello word")
+})
+app.get("/home", (req, res) => {
+    res.send("hello Hazi_anhh")
+})
+app.listen(PORT, () => {
+    console.log(`My app is a funning on port ${PORT}`)
+});
+
