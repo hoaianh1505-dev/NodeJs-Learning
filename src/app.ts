6a8ae8bfb7cfb7e86ca31fc9765f857d0bdf5fb6
Do Hoai Anh <anhd78428@gmail.com>
2026-01-03 19:53:17 +0700

    connection DB to nodejs

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import 'dotenv/config';
 import webRoutes from "./routes/web";
+import getConnection from "./config/database";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -18,6 +19,7 @@ app.use(express.static('public'))
 //config routes
 webRoutes(app);
 
+getConnection();
 app.listen(PORT, () => {
     console.log(`My app is a funning on Port ${PORT}`)
 });
