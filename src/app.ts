db6ca863b26161b95b04852a7d80d9d119f93945
Do Hoai Anh <anhd78428@gmail.com>
2026-01-03 14:09:16 +0700

    set up .env

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
+import 'dotenv/config';
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.get("/", (req, res) => {
     res.send("hello word update nodemon")
 })
@@ -9,5 +10,6 @@ app.get("/hazi_anhh", (req, res) => {
 })
 app.listen(PORT, () => {
     console.log(`My app is a funning on port ${PORT}`)
+    console.log("My port is in:", process.env.PORT)
 });
 
