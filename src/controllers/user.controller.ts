569fc18ff807e5cf41cc2672d36092971523a020
Do Hoai Anh <anhd78428@gmail.com>
2026-01-03 15:50:22 +0700

    use controller and bootstrap

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,9 @@
+import { CreateOptions } from './../../node_modules/ts-node/dist/index.d';
+import { Request, Response } from "express";
+const getHomePage = (req: Request, res: Response) => {
+    return res.render("home")
+}
+const getCreateUserPage = (req: Request, res: Response) => {
+    return res.render("create-user")
+}
+export { getHomePage, getCreateUserPage };
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,18 +1,11 @@
 import express, { Express } from 'express'
+import { getCreateUserPage, getHomePage } from '../controllers/user.controller';
 
 const router = express.Router();
 const webRoutes = (app: Express) => {
-    router.get("/", (req, res) => {
-        res.render("home.ejs")
-    })
+    router.get("/", getHomePage)
+    router.get("/create-user", getCreateUserPage)
 
-    router.get("/hazi_anhh", (req, res) => {
-        res.send("hello Hazi_anhh dep try")
-    })
-
-    router.get("/abc", (req, res) => {
-        res.send("hello abc")
-    })
     app.use("/", router)
 }
 export default webRoutes;
